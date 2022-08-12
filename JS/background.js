7dfd819f8d728c8df17e838f585d9c3f255f164d
Jungbin-Ahn <65687288+Jungbin-Ahn@users.noreply.github.com>
2022-08-12 03:51:20 -0400

    Update background.js

--- JS/background.js
+++ JS/background.js
@@ -1,6 +1,6 @@
 const bg = document.querySelector("body");
 
-bg.style.backgroundImage = "url(photo/bg.jpg)";
+bg.style.backgroundImage = "url(image/bg.jpg)";
 bg.style.backgroundSize = "cover";
 bg.style.backgroundRepeat = "no-repeat";
 bg.style.height = "100vh";
