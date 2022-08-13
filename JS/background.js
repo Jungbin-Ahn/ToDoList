8f1eba25ded49dda892534a6fd761ec968bc3365
Jungbin-Ahn <65687288+Jungbin-Ahn@users.noreply.github.com>
2022-08-13 06:45:41 -0400

    1

--- JS/background.js
+++ JS/background.js
@@ -1,7 +1,1 @@
-const bg = document.querySelector("body");
 
-bg.style.backgroundImage = "url(photo/bg.jpg)";
-bg.style.backgroundSize = "cover";
-bg.style.backgroundRepeat = "no-repeat";
-bg.style.height = "100vh";
-bg.style.backgroundPosition = "center";
